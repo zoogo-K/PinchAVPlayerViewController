735ba7febebd689c26df35dd359284b09ec4f322
k <keke_dev@163.com>
2024-03-26 14:32:52 +0800

    can not scale to lessthan 1

--- PinchAVPlayerViewController/PinchAVPlayerViewController.swift
+++ PinchAVPlayerViewController/PinchAVPlayerViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 import AVKit
 
+import UIKit
+import AVKit
+
 class VideoPlayerViewController: AVPlayerViewController {
     
-    convenience init(urlString: String) {
+    convenience init(fileUrl: String) {
         self.init()
-        player = AVPlayer(url: URL(string: urlString)!)
+        player = AVPlayer(url: URL(fileURLWithPath: fileUrl))
         player?.play()
     }
     
@@ -42,20 +45,33 @@ class VideoPlayerViewController: AVPlayerViewController {
     
     @objc private func handlePinchGesture(_ gesture: UIPinchGestureRecognizer) {
         guard let vv = view.viewWithTag(1000) else { return }
-
-        if gesture.state == .began || gesture.state == .changed {
-            vv.layer.transform = CATransform3DScale(vv.layer.transform, gesture.scale, gesture.scale, 0)
-            gesture.scale = 1
-        }
+        if vv.layer.transform.m11 < 1 && gesture.scale < 1 { return }
+        vv.layer.transform = CATransform3DScale(vv.layer.transform, gesture.scale, gesture.scale, 0)
+        gesture.scale = 1
     }
-
     
     @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: self.view)
         guard let vv = view.viewWithTag(1000) else { return }
+        if vv.layer.transform.m11 <= 1 { return }
+        let newCenter = CGPoint(x: vv.layer.position.x + translation.x, y: vv.layer.position.y + translation.y)
+        let screenBounds = UIScreen.main.bounds
+        var newX = newCenter.x
+        var newY = newCenter.y
+        if newX < screenBounds.width-(vv.layer.transform.m11*screenBounds.width)/2 {
+            newX = screenBounds.width-(vv.layer.transform.m11*screenBounds.width)/2
+        }
+        if newX > (vv.layer.transform.m11*screenBounds.width)/2 {
+            newX = (vv.layer.transform.m11*screenBounds.width)/2
+        }
+        if newY < (vv.layer.transform.m11*screenBounds.width*9/16)/2 {
+            newY = (vv.layer.transform.m11*screenBounds.width*9/16)/2
+        }
+        if newY > screenBounds.height-(vv.layer.transform.m11*screenBounds.width*9/16)/2 {
+            newY = screenBounds.height-(vv.layer.transform.m11*screenBounds.width*9/16)/2
+        }
         if gesture.state == .began || gesture.state == .changed {
-            let newCenter = CGPoint(x: vv.layer.position.x + translation.x, y: vv.layer.position.y + translation.y)
-            vv.layer.position = newCenter
+            vv.layer.position = CGPoint(x: newX, y: newY)
             gesture.setTranslation(CGPoint.zero, in: self.view)
         }
     }
